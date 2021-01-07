423a673b1dcadf9ffb195d264da58e51a69b7196
Ian Epperson <ian@epperson.com>
2021-01-07 15:35:05 -0800

    Get the Github tests working correctly

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-import setuptools
+from setuptools import setup, find_packages
 
 with open('README.md', 'r', encoding='utf-8') as readme:
     long_description = readme.read()
 
 
-setuptools.setup(
+setup(
     name='filestorage',
     version='0.0.1',
     author='Ian Epperson',
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/ianepperson/filestorage',
-    packages=['filestorage'],
+    packages=find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
@@ -22,15 +22,15 @@ setuptools.setup(
     python_requires='>=3.5',
     platforms='any',
     install_requires=[],
-    tests_require=[
-        'pytest',
-        'mock',
-        'pytest-asyncio',
-        'pytest-mock',
-    ],
     extras_require={
         'aio_file': ['aiofiles'],
         's3': ['boto3'],
         'aio_s3': ['aioboto3'],
+        'test': [
+            'pytest',
+            'mock',
+            'pytest-asyncio',
+            'pytest-mock',
+        ],
     },
 )
