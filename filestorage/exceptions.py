fcea286dc6df6eb778de0526335b8e6551ece6c3
Ian Epperson <ian@epperson.com>
2021-01-08 18:59:27 -0800

    Check all files with Black

--- filestorage/exceptions.py
+++ filestorage/exceptions.py
@@ -1,18 +1,22 @@
 class FilestorageError(RuntimeError):
     """Base class for all errors in this library"""
+
     pass
 
 
 class FileNotAllowed(FilestorageError):
     """The provided file is not allowed."""
+
     pass
 
 
 class FileExtensionNotAllowed(FilestorageError):
     """The provided file extension is not allowed."""
+
     pass
 
 
 class FilestorageConfigError(FilestorageError):
     """Error in the configuration."""
+
     pass
